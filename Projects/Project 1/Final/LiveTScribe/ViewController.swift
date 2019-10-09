61cca1c10c4d7f562395474248ef015d49a8a26f
Michelle Galetti <michelle.galetti@gmail.com>
2019-10-09 08:14:46 -0600

    Project 1

---
+++ Projects/Project 1/Final/LiveTScribe/ViewController.swift
@@ -0,0 +1,188 @@
+//
+//  ViewController.swift
+//  LiveTScribe
+//
+//  Created by Michelle Galetti on 9/24/19.
+//  Copyright © 2019 Michelle Galetti. All rights reserved.
+//
+import UIKit
+import Speech
+
+public class ViewController: UIViewController,  SFSpeechRecognizerDelegate {
+    // MARK: Properties
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    private let audioEngine = AVAudioEngine()
+    
+    @IBOutlet weak var transcribedText: UITextView!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var selectToolbar: UIToolbar!
+    
+    public func textViewShouldReturn(textView: UITextView) -> Bool {
+        textView.resignFirstResponder()
+        return true
+    }
+    
+    
+    // MARK: View Controller Lifecycle
+    
+    override public func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        // Disable the record buttons until authorization has been granted.
+        recordButton.isEnabled = false
+    }
+    
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+
+    override public func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Configure the SFSpeechRecognizer object already
+        // stored in a local member variable.
+        speechRecognizer.delegate = self
+        
+        // Asynchronously make the authorization request.
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            
+            // Divert to the app's main thread so that the UI
+            // can be updated.
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    self.recordButton.isEnabled = true
+                case .denied:
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("User denied access to speech recognition", for: .disabled)
+                    
+                case .restricted:
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("Speech recognition restricted on this device", for: .disabled)
+                    
+                case .notDetermined:
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("Speech recognition not yet authorized", for: .disabled)
+                    
+                default:
+                    self.recordButton.isEnabled = false
+                }
+            }
+        }
+    }
+    
+    
+    private func startRecording() throws {
+        // Source code for project: developer.apple.com/documentation/speech
+        // Cancel the previous task if it's running.
+        recognitionTask?.cancel()
+        self.recognitionTask = nil
+        
+        // Configure the audio session for the app.
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        let inputNode = audioEngine.inputNode
+        
+        // Create and configure the speech recognition request.
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to create a SFSpeechAudioBufferRecognitionRequest object") }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        // Create a recognition task for the speech recognition session.
+        // Keep a reference to the task so that it can be canceled.
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            
+            if let result = result {
+                // Update the text view with the results.
+                self.transcribedText.text = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+                print("Text \(result.bestTranscription.formattedString)")
+            }
+            
+            if error != nil || isFinal {
+                // Stop recognizing speech if there is a problem.
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.recordButton.isEnabled = true
+                self.recordButton.setImage(UIImage(named: "logo.png"), for: [])
+            }
+        }
+        
+        // Configure the microphone input.
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+        // Let the user know to start talking.
+        transcribedText.text = "(Go ahead, I'm listening)"
+    }
+    
+    // MARK: SFSpeechRecognizerDelegate
+    
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            recordButton.isEnabled = true
+            recordButton.setImage(UIImage(named: "logo.png"), for: [])
+        } else {
+            recordButton.isEnabled = false
+            recordButton.setTitle("Recognition Not Available", for: .disabled)
+        }
+    }
+    
+    // MARK: Interface Builder actions
+    
+    
+    @IBAction func recordButtonTapped(_ sender: Any) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            recordButton.isEnabled = false
+            recordButton.setImage(UIImage(named: "stop.png"), for: .disabled)
+        } else {
+            do {
+                try startRecording()
+                recordButton.setImage(UIImage(named: "stop.png"), for: [])
+            } catch {
+                recordButton.setTitle("Recording Not Available", for: [])
+            }
+        }
+    }
+    @objc func shareTapped(_ sender: Any) {
+        guard let myText : String = transcribedText.text
+            else{
+                print("no image found")
+                return
+        }
+        let vc = UIActivityViewController(activityItems: [myText], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+    @IBAction func deleteAll(_ sender: Any) {
+        transcribedText.text = "Previous conversation removed."
+    }
+    
+}
---
+++ Projects/Project 1/Rough Draft/LiveTScribe/viewController+Style.swift
@@ -0,0 +1,64 @@
+//
+//  viewController+Style.swift
+//  LiveTScribe
+//
+//  Created by Michelle Galetti on 9/29/19.
+//  Copyright © 2019 Michelle Galetti. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum SpeechStatus {
+    case ready
+    case recognizing
+    case unavailable
+}
+
+// MARK: - UI Styling
+extension ViewController {
+    
+    func applyStyle() {
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0, green: 0.8034699559, blue: 0.789139688, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barStyle = .black
+    }
+    
+    func setUI(status: SpeechStatus) {
+        switch status {
+        case .ready:
+            microphoneButton.setImage(#imageLiteral(resourceName: "available"), for: .normal)
+        case .recognizing:
+            microphoneButton.setImage(#imageLiteral(resourceName: "stop"), for: .normal)
+        case .unavailable:
+            microphoneButton.setImage(#imageLiteral(resourceName: "unavailable"), for: .normal)
+        }
+    }
+    
+    func searchFlight(number: String) {
+        if let flight = FlightsDataSource.searchFlight(number: number) {
+            flightTextView.text = "\(number)\n\(flight.status)"
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return FlightsDataSource.flights.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FlightCell", for: indexPath)
+        let flight = FlightsDataSource.flights[indexPath.row]
+        cell.textLabel?.text = flight.number
+        cell.detailTextLabel?.text = flight.status
+        return cell
+    }
+}
